317e2f7c51a72eaf9d6700c6d40bfbef94cf58cc
Hanoit <j-deiby@hotmail.com>
2015-02-06 11:06:17 -0500

    primer comit

---
+++ js/employeelist.js
@@ -0,0 +1,22 @@
+var serviceURL = "http://serviceandroid.zz.vc/";
+
+var employees;
+
+$('#employeeListPage').bind('pageinit', function(event) {
+	getEmployeeList();
+});
+
+function getEmployeeList() {
+	$.getJSON(serviceURL + 'getemployees.php', function(data) {
+		$('#employeeList li').remove();
+		employees = data.items;
+		$.each(employees, function(index, employee) {
+			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+					'<img src="http://serviceandroid.zz.vc/pics/' + employee.picture + '"/>' +
+					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+					'<p>' + employee.title + '</p>' +
+					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+		});
+		$('#employeeList').listview('refresh');
+	});
+}
